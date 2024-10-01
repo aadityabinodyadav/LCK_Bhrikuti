f0ec0e713f92438cf9b0003ca31449e86150bbe9
Aaditya B. Yadav <aadityarayyadav@gmail.com>
2024-10-01 21:09:30 +0545

    starter

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import DropdownMenu from './Dropdown'
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+  
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import leo from './assets/leo.png'
+
+export default  function Navbar(){
+    return (
+       <nav>
+        <img src={leo} className="w-16" />
+        
+       </nav>
+    )
+}
